4d52a6a1348266cfb42bcb83545837a87b7b555d
maslio <vlad@maslio.com>
2024-05-16 14:34:51 +0700

    chore: Add Nuxt plugin for device and maska imports

--- src/runtime/base/plugins/device.ts
+++ src/runtime/base/plugins/device.ts
@@ -1,4 +1,5 @@
 import { kebabCase } from 'scule'
+import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin(({ $device }) => {
   const device = $device as Record<string, boolean>
--- src/runtime/ui/plugins/maska.ts
+++ src/runtime/ui/plugins/maska.ts
@@ -1,4 +1,5 @@
 import { vMaska } from 'maska'
+import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('maska', vMaska)
