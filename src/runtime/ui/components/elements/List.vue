7cd3c56e8712fa4d43ccae1b3f0fcec59666444b
maslio <vlad@maslio.com>
2024-05-31 15:31:57 +0700

    BREAKING CHANGE(ui): change List.vue props

--- src/runtime/ui/components/elements/List.vue
+++ src/runtime/ui/components/elements/List.vue
@@ -4,31 +4,29 @@ import InputString from '../inputs/InputString.vue'
 import Separator from './Separator.vue'
 import Item from './Item.vue'
 import Card from './Card.vue'
-import { type Ref, computed, onKeyStroke, ref, toReactive, watch, watchDebounced } from '#imports'
-
-type Items = T[] | { total: number, items: T[] }
-type ItemsFn = (input: string, limit: number, items: T[]) => Items | Promise<Items>
+import type { Ref, useAsyncData } from '#imports'
+import type { NuxtError } from '#app'
+import { computed, onKeyStroke, ref, toReactive, useNuxtApp, useToNumber, watchDebounced } from '#imports'
 
 const props = withDefaults(defineProps<{
   label?: string
   caption?: string
-  total?: boolean
-  items?: Items | ItemsFn
-  keys: string & keyof T
+  data?: ReturnType<typeof useAsyncData<T[] | null, NuxtError | null>>
+  total?: ReturnType<typeof useAsyncData<number | null, NuxtError | null>>
+  items?: T[]
+  itemKey: string & keyof T
   limit?: number
-  limitIncrement?: number
-  input?: boolean
-  inputValue?: string
-  inputFocus?: boolean
-  inputLabel?: string
-  inputDebounce?: number
-  query?: any
-  component?: any
+  increment?: number
+  debounce?: string | number
+  search?: string
 }>(), {
   limit: 5,
-  inputLabel: 'Search',
-  inputDebounce: 300,
+  increment: 5,
 })
+const emit = defineEmits([
+  'update:search',
+  'update:limit',
+])
 defineSlots<{
   default: (props: {
     item: T
@@ -37,78 +35,52 @@ defineSlots<{
   }) => any
   label: () => any
 }>()
+const $click = useNuxtApp().$click as string
+const debounce = useToNumber(() => props.debounce ?? 0)
 
-const input = defineModel<string>({ default: '' })
-watch(() => props.inputValue, (value) => {
-  if (value != null)
-    input.value = value
-}, { immediate: true })
+const input = ref(props.search ?? '')
+watchDebounced(input, (value) => {
+  emit('update:search', value)
+  focus.first()
+}, { debounce })
 
 const list = ref<HTMLDivElement>()
 
-const data = (function () {
-  const limit = ref(props.limit)
-  const items = ref<T[]>([]) as Ref<T[]>
-  const total = ref<number>(0)
-  const pending = ref(false)
+const items = computed<T[]>(() => {
+  if (props.data)
+    return props.data.data.value as T[]
+  if (props.items)
+    return props.items
+  return []
+})
 
-  function setItems(data: Items) {
-    if (Array.isArray(data)) {
-      items.value = data
-      total.value = data.length
-    }
-    else {
-      items.value = data.items
-      total.value = data.total
-    }
-  }
+const total = computed(() => {
+  if (!props.total)
+    return null
+  return props.total.data.value
+})
 
-  let fetchId: number = 0 // prevent outdated fetch results
-  async function fetch(silent = false) {
-    if (!silent)
-      pending.value = true
-    if (props.items) {
-      if (typeof props.items === 'function') {
-        const id = ++fetchId
-        const data = await props.items(input.value, limit.value, items.value)
-        if (id === fetchId)
-          setItems(data)
-        else
-          return
-      }
-      else { setItems(props.items) }
-    }
-    pending.value = false
-  }
-
-  const increment = computed(() => props.limitIncrement ?? props.limit)
-  const hasMore = computed<number>(() => {
-    if (!items.value.length)
-      return 0
-    if (total.value > items.value.length) {
-      const more = total.value - items.value.length
-      if (more > increment.value)
-        return increment.value
-      return more
-    }
+const hasMore = computed<number>(() => {
+  if (!items.value?.length)
     return 0
-  })
-  async function increaseLimit() {
-    focus.remember()
-    limit.value = limit.value + increment.value
-    await fetch()
-    focus.restore()
+  if (total.value && total.value > items.value.length) {
+    const more = total.value - items.value.length
+    if (more > props.increment)
+      return props.increment
+    return more
   }
-  watch(props, () => fetch())
-  watchDebounced(input, () => fetch(), { debounce: props.inputDebounce })
-  return toReactive({ input, items, total, pending, fetch, increment, hasMore, increaseLimit })
-})()
+  return 0
+})
+async function increaseLimit() {
+  focus.remember()
+  emit('update:limit', props.limit + props.increment)
+}
 
 const focus = (function () {
   const active = ref(false)
   const index = ref<number>(-1)
   function focus(i: number) {
-    if (data.items[i] || (data.hasMore && data.items.length === i))
+    if (items.value[i] || (hasMore.value && items.value.length === i))
       index.value = i
   }
   function next() {
@@ -122,7 +94,7 @@ const focus = (function () {
   }
   function select() {
     const clickableEl = list.value?.querySelector('.focused .clickable') as HTMLElement
-    clickableEl?.click()
+    clickableEl?.dispatchEvent(new MouseEvent($click))
   }
   const rememberedIndex = ref(index.value)
   function remember() {
@@ -146,9 +118,6 @@ const focus = (function () {
   })
   return toReactive({ active, index, first, remember, restore })
 })()
-
-defineExpose({ fetch: data.fetch })
-await data.fetch()
 </script>
 
 <template>
@@ -158,61 +127,51 @@ await data.fetch()
       class="list overflow-hidden rounded-xl card:rounded-none dialog:rounded-none"
       color="default"
     >
-      <template v-if="$props.input">
+      <template v-if="$props.search != null">
         <InputString
           v-model="input"
-          :placeholder="inputLabel"
+          :placeholder="$t('search')"
           flat
           @focus="focus.active = true"
           @blur="focus.active = false"
         >
-          <div v-if="data.pending">
+          <div v-if="$props.data?.status.value === 'pending'">
             <Icon name="spinner" animate-spin />
           </div>
           <div
-            v-else-if="input" flex
-            items-center
-            text-faint
+            v-else-if="input"
+            class="flex items-center text-faint"
           >
             <ItemButton icon="close" @click="input = ''" />
           </div>
         </InputString>
         <Separator />
       </template>
-      <template v-if="data.items.length">
+      <template v-if="items?.length">
         <div
-          v-for="(item, index) in data.items"
-          :key="item[props.keys] as string"
+          v-for="(item, index) in items"
+          :key="String(item[props.itemKey])"
           :class="{ focused: (focus.active && index === focus.index) }"
           class="flex children:flex-1"
         >
-          <component
-            :is="component" v-if="props.component"
-            :item
-            :index
-          />
-          <slot v-else :item="item" :index />
+          <slot :item="item" :index />
         </div>
       </template>
       <template v-else>
-        <div
-          flex flex-col
-          items-center
-          justify-center p-3
-        >
+        <div class="flex flex-col items-center justify-center p-3">
           <Icon name="tabler:mood-empty" size="40" />
           <div text-faint>
             No data
           </div>
         </div>
       </template>
-      <template v-if="data.hasMore">
+      <template v-if="hasMore">
         <Separator />
-        <div :class="{ focused: (focus.active && data.items.length === focus.index) }">
-          <Item clickable @click="data.increaseLimit">
+        <div :class="{ focused: (focus.active && items.length === focus.index) }">
+          <Item clickable @click="increaseLimit">
             <template #main>
               <div text-center>
-                Show {{ data.hasMore }} more
+                Show {{ hasMore }} more
               </div>
             </template>
           </Item>
@@ -221,10 +180,9 @@ await data.fetch()
     </div>
   </Card>
   <div
-    v-if="props.total" m-2
-    text-center
-    text-faint
+    v-if="total"
+    class="m-2 text-center text-faint"
   >
-    Total: {{ data.total }}
+    Total: {{ total }}
   </div>
 </template>
